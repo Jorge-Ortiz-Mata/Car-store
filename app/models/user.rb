dc56e6ce3567475d4f9c834c3b9d0f617830fe36
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-04-01 12:03:04 -0600

    Adding devise and omniauth with facebook

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+
+ def self.from_omniauth(auth)
+   name_split = auth.info.name.split(" ")
+   user = User.where(email: auth.info.email).first
+   user ||= User.create!(provider: auth.provider, uid: auth.uid, email: auth.info.email, password: '123456')
+   user
+ end  
+end
