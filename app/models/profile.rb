957f966aa159e3a0bfbc67f0f0da123e994b4738
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-04-03 11:40:55 -0500

    Adding validations to user model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,20 @@
 class Profile < ApplicationRecord
+
+    # Associations.
+
+# ----------------------------------- ASSOCIATIONS -------------------------------
+
     belongs_to :user
+
+# ----------------------------------- VALIDATIONS -------------------------------
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :birth, presence: true
+    validates :location, presence: true
+    validates :user_id, presence: true
+
+# ----------------------------------- FRIENDLY GEM -------------------------------
 
     extend FriendlyId
     friendly_id :first_name, use: :slugged
