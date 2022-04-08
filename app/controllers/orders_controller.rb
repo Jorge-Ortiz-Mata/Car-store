7cc3497a72bd4a42da2af898cde294dc1e484bba
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-04-08 10:15:57 -0500

    Fixing views and adding buttons to buy or remove in My Cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,15 +9,19 @@ class OrdersController < ApplicationController
 
     def add_to_cart
         Order.create(user_id: @user.id, vehicle_id: @vehicle.id)        
-        redirect_to root_path, notice: "Product added to your cart. User: #{@user.id}, Vehicle: #{@vehicle.id}"
-        
+        redirect_to vehicle_path(@vehicle), notice: "Product added to your cart." 
+    end
+
+    def buy_product
+    end
+
+    def remove_product
     end
 
     private
-    def set_profile_and_vehicle
-      @vehicle = Vehicle.friendly.find(params[:vehicle_id])
-      @user = User.find(params[:user_id])
-    end
 
-
+        def set_profile_and_vehicle
+            @vehicle = Vehicle.friendly.find(params[:vehicle_id])
+            @user = User.find(params[:user_id])
+        end
 end
