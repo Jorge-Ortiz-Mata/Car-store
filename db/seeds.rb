dd678c9901ee0aa3235bc1649e54c25a77761262
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-04-10 14:53:14 -0500

    Modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,25 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+users_all_relationship = User.all
+users_id_array = Array.new
+for i in 0...User.all.length
+    users_id_array[i] = users_all_relationship[i].id
+end
 
-100.times do
+categories_all_relationship = Category.all
+categories_id_array = Array.new
+for i in 0...Category.all.length
+    categories_id_array[i] = categories_all_relationship[i].id
+end
+
+30.times do
     Vehicle.create!([{
         title: Faker::Vehicle.car_type,
         description: Faker::Vehicle.standard_specs,
         price: rand(3000.0..10000.00).round(2),
-        category_ids: rand(1..6),
+        category_ids: rand(categories_id_array.min..categories_id_array.max),
         company: Faker::Vehicle.make,
-        user_id: rand(5..11)
+        user_id: rand(users_id_array.min..users_id_array.max)
     }])
 end
