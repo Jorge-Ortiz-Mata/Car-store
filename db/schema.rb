0f6bc83a8effab240eff49632e40beaa74ec02a2
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-04-10 15:01:41 -0500

    Fixing commits (no important)

--- db/schema.rb
+++ db/schema.rb
@@ -121,15 +121,8 @@ ActiveRecord::Schema.define(version: 2022_04_09_231520) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "provider"
     t.string "uid"
-    t.string "slug"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
   create_table "vehicle_categories", force: :cascade do |t|
