2a913813d42af524bbfc23add46b36bd79862577
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-04-12 10:16:58 -0500

    Adding gem pundit to posts

---
+++ app/policies/post_policy.rb
@@ -0,0 +1,13 @@
+class PostPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def new?
+    @user.profile.position.name.include? 'member'
+  end
+
+end
