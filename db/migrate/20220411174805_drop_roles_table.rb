5a286978edb6dd75c1ced542c3e76f06e01fa447
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-04-12 00:56:51 -0500

    Removing roles tables migrations.

--- db/migrate/20220411174805_drop_roles_table.rb
+++
@@ -1,9 +0,0 @@
-class DropRolesTable < ActiveRecord::Migration[6.1]
-  def up
-    drop_table :roles
-  end
-
-  def down
-    fail ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20220411175031_drop_users_roles_table.rb
+++
@@ -1,9 +0,0 @@
-class DropUsersRolesTable < ActiveRecord::Migration[6.1]
-  def up
-    drop_table :users_roles
-  end
-
-  def down
-    fail ActiveRecord::IrreversibleMigration
-  end
-end
