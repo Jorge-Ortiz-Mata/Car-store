6c4c8bdc211de14815746a8635903fb5f1f9eec1
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-04-13 13:38:08 -0500

    Updating vehicle separating form

--- app/controllers/vehicles/vehicle_wizard_controller.rb
+++ app/controllers/vehicles/vehicle_wizard_controller.rb
@@ -4,9 +4,21 @@ class Vehicles::VehicleWizardController < ApplicationController
 
     steps :name_and_files, :description_and_price
     before_action :set_vehicle
+    before_action :vehicle_params, only: [:update]
 
     def show
         render_wizard
+    end
+    
+    def update
+        case step
+            when :name_and_files
+                @vehicle.update(vehicle_params)
+
+            when :description_and_price
+                @vehicle.update(vehicle_params)
+        end
+        render_wizard @vehicle
     end
 
     def finish_wizard_path
@@ -14,7 +26,12 @@ class Vehicles::VehicleWizardController < ApplicationController
     end
 
     private 
+
         def set_vehicle
             @vehicle = Vehicle.friendly.find(params[:vehicle_id])
+        end
+
+        def vehicle_params
+            params.require(:vehicle).permit(:title, :description, :category_ids, :price, :company, :video, :video_thumbnail, images: [])
         end
 end
