7320bb54ed611b02b592ae5661ff056fca02490b
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-04-15 13:58:32 -0500

    Adding method to check existing reviews.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ApplicationRecord
-  belongs_to :user
-  belongs_to :vehicle
+    belongs_to :user
+    belongs_to :vehicle
+
+    private
+
+        def self.look_up(user, vehicle)
+            where(user_id: user.id, vehicle: vehicle.id).first.present?
+        end
+
 end
